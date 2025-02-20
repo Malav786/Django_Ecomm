e50e605e09f2cd3f1f5a292f0141b28c8a32ba57
Malav <20it01>
2025-02-20 00:22:55 -0500

    cart summary finished-showing quantities

--- cart/cart.py
+++ cart/cart.py
@@ -1,3 +1,5 @@
+from store.models import Product
+
 class Cart():
     def __init__(self, request):
         self.session = request.session
@@ -9,3 +11,30 @@ class Cart():
         
         # make sure cart is available on all pages of the site
         self.cart = cart
+    
+    def add(self, product, quantity):
+        product_id = str(product.id)
+        product_qty = str(quantity)
+        # Logic
+        if product_id in self.cart:
+            pass
+        else:
+            #self.cart[product_id] = {'price': str(product.price)}
+            self.cart[product_id] = int(product_qty)
+        self.session.modified = True
+
+    def __len__(self):
+        return len(self.cart) 
+    
+    def get_prods(self):
+        # Get ids from the cart
+        product_ids = self.cart.keys()
+        # use ids to lookup products in the database
+        products = Product.objects.filter(id__in=product_ids)
+
+        return products
+    
+    def get_quants(self):
+        # Get ids from the cart
+        quantities = self.cart
+        return quantities
--- cart/views.py
+++ cart/views.py
@@ -1,11 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .cart import Cart
+from store.models import Product
+from django.http import JsonResponse
+
+
 
 # Create your views here.
 def cart_summary(request):
-    return render(request, 'cart_summary.html', {})
+    # get the cart object
+    cart = Cart(request)
+    cart_products = cart.get_prods
+    quantities = cart.get_quants
+    return render(request, 'cart_summary.html', {"cart_products":cart_products, "quantities":quantities})
 
 def cart_add(request):
-    pass
+    # get the cart
+    cart = Cart(request)
+    # test for post request
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('product_id'))
+        product_qty = int(request.POST.get('product_qty'))
+        # lookup product in database
+        product = get_object_or_404(Product, id=product_id)
+        # save to session
+        cart.add(product=product, quantity=product_qty)
+
+        # Get the cart quantity
+        cart_quantity = cart.__len__()
+
+        # return response
+        #response = JsonResponse({'Product Name: ': product.name})
+        response = JsonResponse({'qty: ': cart_quantity})
+        return response
 
 def cart_delete(request):
     pass
